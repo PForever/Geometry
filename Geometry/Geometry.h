2240c17dbcd1d23327e36a0ee5a3e1d8426a05b2
Igor Ganov <igor_ganov@live.ru>
2017-12-01 12:48:22 +0300

    Добавьте файлы проекта.

---
+++ Geometry/Geometry.h
@@ -0,0 +1,13 @@
+// Geometry.h
+
+#pragma once
+
+using namespace System;
+
+namespace Geometry {
+
+	public ref class Class1
+	{
+		// TODO: здесь следует добавить свои методы для этого класса.
+	};
+}
